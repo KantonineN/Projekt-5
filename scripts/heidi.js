f4694d31db915d2b8fd67cdd8e68753727a030f9
KantonineN <nbg.100382@gmail.com>
2024-12-12 10:32:23 +0100

    Accordion JS done

--- scripts/heidi.js
+++ scripts/heidi.js
@@ -1,18 +1,21 @@
-const acc = document.getElementsByClassName("accordion");
+function Accordion() {
+    const acc = document.getElementsByClassName("accordion__btn");
 
-for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        
-        const panel = this.nextElementSibling;
-        const arrow = this.querySelector(".accordion__arrow");
+    for (let i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            
+            const panel = this.nextElementSibling;
+            const arrow = this.querySelector(".accordion__arrow");
 
-        if (panel.classList.contains("open")) {
-            panel.classList.remove("open");
-            arrow.style.transform = "rotate(-45deg)";
-        } else {
-            panel.classList.add("open");
-            arrow.style.transform = "rotate(45deg)"; 
-        }
-    });
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+                arrow.style.transform = "translateY(-50%) rotate(45deg)";
+            } else {
+                panel.style.display = "block";
+                arrow.style.transform = "translateY(-50%) rotate(-135deg)";
+            }
+        });
+    }
 }
+Accordion();
