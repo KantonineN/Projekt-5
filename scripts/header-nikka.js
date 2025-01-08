6892c1f8ac2d8235634ef11816fa51b4dd0db95b
KantonineN <nbg.100382@gmail.com>
2025-01-08 16:33:50 +0100

    header + studie rettet

--- scripts/header-nikka.js
+++ scripts/header-nikka.js
@@ -1,11 +1,23 @@
+/* Load SVG's */
 $(document).ready(function () {
     $("#svg-content").load("injects/svg.html");
 });
+$(document).ready(function () {
+    $("#svg-content").load("../injects/svg.html");
+});
 
+/* Load header for large devices */
 $(document).ready(function () {
     $("#header-content-xl").load("injects/header-xl.html");
 });
+$(document).ready(function () {
+    $("#header-content-xl").load("../injects/header-xl--sub.html");
+});
 
+/* Load header for devices */
 $(document).ready(function () {
     $("#header-content").load("injects/header.html");
+});
+$(document).ready(function () {
+    $("#header-content").load("../injects/header--sub.html");
 });
