d3d6f2c7f8fb1d57d927ce4cee311c0b67cc2ac7
KantonineN <nbg.100382@gmail.com>
2025-01-15 11:01:54 +0100

    Projektbank rettet

---
+++ scripts/snackbar-nikka.js
@@ -0,0 +1,7 @@
+function showSnackbar() {
+    console.log("Loaded");
+    const submit = document.getElementById("submit");
+    submit.addEventListener("click", function() {
+    });
+}
+showSnackbar();
